13702c66121f55abfabc88c9dc9a2088fe024685
Will Foxall <wfoxall@protonmail.com>
2025-11-23 22:27:03 +0000

    Separated out ArmySelect component

--- src/components/ArmyControls.vue
+++ src/components/ArmyControls.vue
@@ -4,18 +4,10 @@
         <div class="grid-cols-2">
             <div class="flex flex-col">
                 <UFormField label="Team A">
-                    <USelectMenu v-model="armyA" class="min-w-80" :items="AllArmyConfigs" placeholder="Select Army" :filter-fields="['detachment','faction','superfaction']">
-                        <template #item-label="{item}">{{ item.detachment }}</template>
-                        <template #item-description="{item}">{{ item.faction }} ({{ item.superfaction }})</template>
-                        <template #item-leading="{item}"><UAvatar v-if="item.icon" :src="`./avatars/${item.icon}`"/></template>
-                    </USelectMenu>
+                    <ArmySelect v-model="armyA"/>
                 </UFormField>
                 <UFormField label="Team B">
-                    <USelectMenu v-model="armyB" class="min-w-80" :items="AllArmyConfigs" placeholder="Select Army" :filter-fields="['detachment','faction','superfaction']">
-                        <template #item-label="{item}">{{ item.detachment }}</template>
-                        <template #item-description="{item}">{{ item.faction }} ({{ item.superfaction }})</template>
-                        <template #item-leading="{item}"><UAvatar v-if="item.icon" :src="`./avatars/${item.icon}`"/></template>
-                    </USelectMenu>
+                    <ArmySelect v-model="armyB"/>
                 </UFormField>
             </div>
         </div>
@@ -25,6 +17,6 @@
 <script setup lang="ts">
 import { useArmies } from '../composables/useArmies';
 
-const {armyA,armyB, AllArmyConfigs} = useArmies();
+const {armyA,armyB} = useArmies();
 
 </script>
---
+++ src/components/ArmySelect.vue
@@ -0,0 +1,22 @@
+<template>
+    <USelectMenu 
+        v-model="model" 
+        class="min-w-80" 
+        :items="AllArmyConfigs" 
+        placeholder="Select Army"
+        :filter-fields="['detachment','faction','superfaction']"
+        :ui="{content: ['max-h-120']}"
+        >
+        <template #item-label="{item}">{{ item.detachment }}</template>
+        <template #item-description="{item}">{{ item.faction }} ({{ item.superfaction }})</template>
+        <template #item-leading="{item}"><UAvatar v-if="item.icon" :src="`./avatars/${item.icon}`"/></template>
+    </USelectMenu>
+</template>
+
+<script setup lang="ts">
+import { type ArmyConfig, useArmies } from '../composables/useArmies';
+
+const model = defineModel<ArmyConfig>()
+
+const {AllArmyConfigs} = useArmies();
+</script>
