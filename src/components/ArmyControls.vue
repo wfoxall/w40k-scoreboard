d061f474f2b29ed997708a71abbd36d25878ce34
Will Foxall <wfoxall@protonmail.com>
2026-01-03 08:07:17 +0000

    Added checkboxes to set army painted state

--- src/components/ArmyControls.vue
+++ src/components/ArmyControls.vue
@@ -1,21 +1,41 @@
 <template>
     <div class="space-y-4">
         <div class="text-2xl">Pick Armies</div>
-        <div class="grid-cols-2">
+        <div class="grid md:grid-cols-2 gap-6">
             <div class="flex flex-col gap-4">
-                <UFormField label="Team A">
+                <h2 class="text-xl">Player A</h2>
+                <UFormField label="Faction & Detachment">
                     <ArmySelect v-model="armyA"/>
                 </UFormField>
-                <UFormField label="Team B">
+                <UCheckbox v-model="ArmyAPainted" label="Army Painted" />
+            </div>
+            <div class="flex flex-col gap-4">
+                <h2 class="text-xl">Player B</h2>
+                <UFormField label="Faction & Detachment">
                     <ArmySelect v-model="armyB"/>
                 </UFormField>
+                <UCheckbox v-model="ArmyBPainted" label="Army Painted" />
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useArmies } from '../composables/useArmies';
 
-const {armyA,armyB} = useArmies();
+const ArmyAPainted = computed({
+    get: () => {return !!armyA.value.painted},
+    set: (val: boolean) => {
+        setArmyPainted('a',val);
+    }
+})
+const ArmyBPainted = computed({
+    get: () => {return !!armyB.value.painted},
+    set: (val: boolean) => {
+        setArmyPainted('b',val);
+    }
+})
+
+const {armyA,armyB, setArmyPainted} = useArmies();
 </script>
